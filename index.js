c522d9c0575ec8ea8306c18a54ec48c39ece0d76
kamalrommel <kamal.rommel@gmail.com>
2024-10-02 09:56:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const replacementsFilePath = path.join(__dirname, 'replacements.json');
 const levenshtein = require('js-levenshtein');
 const winnaarsFilePath = path.join(__dirname, 'winnaars.json');
 const gebruikersAntwoorden = {}; // Hier worden de correct beantwoorde vragen per gebruiker opgeslagen
+
+require('dotenv').config();
 const token = process.env.DISCORD_TOKEN;
+client.login(token);
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
